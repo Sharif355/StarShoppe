6b69e715a8f484cdd4d6e4169c501a7db129e1e4
Shariful Islam <70468977+Sharif355@users.noreply.github.com>
2023-10-19 13:08:41 +0600

    private routes & errorpage updated

--- src/Error/ErrorPage.jsx
+++ src/Error/ErrorPage.jsx
@@ -1,7 +1,17 @@
+import Lottie from "lottie-react";
+import error from "../../public/error.json";
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Error 404!</h1>
+    <div className="flex flex-col justify-center items-center mt-40 space-y-2 bg- ">
+      <Lottie className=" w-96 md:w-[500px]" animationData={error}></Lottie>
+      <h1 className="text-lg font-semibold">
+        The Page You Looking Doesn&apos;t exist.
+      </h1>
+      <Link to="/">
+        <button className="btn normal-case rounded-lg">Go Back</button>
+      </Link>
     </div>
   );
 };
